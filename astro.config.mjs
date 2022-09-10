a7c8923791d5c74c846504504cad97558329bfa3
Will Nelson <will@wnelson.xyz>
2022-09-09 22:10:17 -0700

    Remove custom CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,31 @@ export default defineConfig({
   integrations: [
     Unocss({
       presets: [presetIcons(), presetUno()],
-      theme: { fontFamily: { serif: ['"Roboto Slab"', 'serif'] } },
+      theme: {
+        fontFamily: { serif: ['"Roboto Slab"', 'serif'] },
+        animation: {
+          durations: {
+            'bg-pulse': '4s',
+          },
+          timingFns: {
+            'bg-pulse': 'ease-in-out',
+          },
+          counts: {
+            'bg-pulse': 'infinite',
+          },
+          keyframes: {
+            'bg-pulse': `{
+              0%,
+              100% {
+                background-position-y: 0%;
+              }
+              50% {
+                background-position-y: 80%;
+              }
+            }`,
+          },
+        },
+      },
     }),
   ],
   site: 'https://wnelson.dev',
