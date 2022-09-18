29e76353812ffa44caa1062d986b258297709c00
Will Nelson <will@wnelson.xyz>
2022-09-18 10:57:47 -0700

    Avoid incorrect sizing calculation

--- src/components/profile/CustomStatus.tsx
+++ src/components/profile/CustomStatus.tsx
@@ -1,27 +1,18 @@
 import type { GatewayActivity } from 'discord-api-types/v10';
-import { createSignal, Match, onMount, Switch } from 'solid-js';
+import { Match, Switch } from 'solid-js';
 
 export interface Props {
 	activity: GatewayActivity;
 }
 
 export default function CustomStatus(props: Props) {
-	const [imgRef, setImgRef] = createSignal<HTMLImageElement>();
 	const extension = () => props.activity.emoji?.animated ? 'gif' : 'png';
-	const [size, setSize] = createSignal<number>();
-
-	onMount(() => {
-		const el = imgRef();
-		let fontSize = el && getComputedStyle(el).fontSize;
-		fontSize = fontSize?.slice(0, fontSize.length - 2);
-		setSize(fontSize ? Math.max(parseFloat(fontSize), 16) : 16);
-	});
 
 	return (
 		<>
 			<Switch fallback={<></>}>
 				<Match when={props.activity.emoji?.id}>
-					<img ref={setImgRef} class='h-4 inline mr-1' src={`https://cdn.discordapp.com/emojis/${props.activity.emoji!.id}.${extension()}?size=${size()}`} />
+					<img class='h-4 inline mr-1' src={`https://cdn.discordapp.com/emojis/${props.activity.emoji!.id}.${extension()}?size=32`} />
 				</Match>
 				<Match when={props.activity.emoji?.name}>
 					<span class='mr-1'>{props.activity.emoji!.name}</span>
