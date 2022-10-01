9afd8521f0c87ffd3a37608bc9268fe3acab5fa4
Will Nelson <me@wnelson.dev>
2022-10-01 00:33:45 -0700

    Add photo hosting (#1)
    * Load photos from bunnycdn
    
    * Decent photos
    
    * Switch domains
    
    * Fail builds when photos don't load
    
    * Use static pages
    
    * Navbar navigation
    
    * Fix types for photos
    
    * Hide next when no items

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,35 +6,38 @@ import Unocss from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), Unocss({
-    presets: [presetIcons(), presetUno()],
-    theme: {
-      fontFamily: {
-        serif: ['"Roboto Slab"', 'serif']
-      },
-      animation: {
-        durations: {
-          'bg-pulse': '4s'
+  integrations: [
+    solidJs(),
+    Unocss({
+      presets: [presetIcons(), presetUno()],
+      theme: {
+        fontFamily: {
+          serif: ['"Roboto Slab"', 'serif']
         },
-        timingFns: {
-          'bg-pulse': 'ease-in-out'
-        },
-        counts: {
-          'bg-pulse': 'infinite'
-        },
-        keyframes: {
-          'bg-pulse': `{
-              0%,
-              100% {
-                background-position-y: 0%;
-              }
-              50% {
-                background-position-y: 80%;
-              }
-            }`
+        animation: {
+          durations: {
+            'bg-pulse': '4s'
+          },
+          timingFns: {
+            'bg-pulse': 'ease-in-out'
+          },
+          counts: {
+            'bg-pulse': 'infinite'
+          },
+          keyframes: {
+            'bg-pulse': `{
+                0%,
+                100% {
+                  background-position-y: 0%;
+                }
+                50% {
+                  background-position-y: 80%;
+                }
+              }`
+          }
         }
       }
-    }
-  })],
+    }),
+  ],
   site: 'https://wnelson.dev'
 });
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,61 @@
+import { autoUpdate, offset, ReferenceElement, shift } from '@floating-ui/dom';
+import { createMemo, createSignal, mergeProps, ParentProps, Show } from 'solid-js';
+import { useFloating } from './util/floating-ui';
+
+export interface Props {
+	links: Record<string, string>;
+	collapseSingle?: boolean;
+}
+
+export default function Dropdown(props: ParentProps<Props>) {
+	props = mergeProps({ collapseSingle: true }, props);
+
+	const [isOpen, setOpen] = createSignal(false);
+	const [ref, setRef] = createSignal<ReferenceElement>();
+	const [floating, setFloating] = createSignal<HTMLElement>();
+	const firstLink = createMemo(() => Object.keys(props.links)[0]);
+	const hasManyLinks = createMemo(() => !Boolean(props.collapseSingle) || Object.keys(props.links).length > 1);
+
+	const position = useFloating(ref, floating, {
+		whileElementsMounted: autoUpdate,
+		middleware: [offset(5), shift()],
+		placement: 'bottom-start',
+	});
+
+	return (
+		<div onClick={() => setOpen(isOpen => hasManyLinks() && !isOpen)}>
+			<div ref={setRef} class='relative transition-colors hover:bg-stone-300 rounded px-1 cursor-pointer'>
+				<Show when={hasManyLinks()} fallback={<a href={firstLink()}>{props.children}</a>}>
+					<div class='flex flex-row'>
+						<span class='mr-2' classList={{
+							hidden: !props.children,
+						}}>{props.children}</span>
+						<span
+							class='inline-block align-middle'
+							classList={{
+								'i-bi-chevron-up': isOpen(),
+								'i-bi-chevron-down': !isOpen(),
+							}}
+						/>
+					</div>
+				</Show>
+			</div>
+			<div
+				ref={setFloating}
+				class='bg-stone-100 p-3 rounded shadow flex-col'
+				classList={{ hidden: !isOpen(), flex: isOpen() }}
+				style={{
+					position: position.strategy,
+					left: `${position.x ?? 0}px`,
+					top: `${position.y ?? 0}px`,
+				}}
+			>
+				<ul>
+					{Object.entries(props.links).map(([url, name]) => (
+						<a class='block transition-colors hover:bg-stone-200 rounded px-1' href={url}>{name}</a>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+}
