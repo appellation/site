7a81743b243050dfd34e16fd50a817baf322a033
Will Nelson <me@wnelson.dev>
2022-10-10 17:48:37 -0700

    Fix profile card on dark theme

--- src/components/FloatingCard.tsx
+++ src/components/FloatingCard.tsx
@@ -25,7 +25,7 @@ export default function FloatingCard<R extends ReferenceElement>(props: ParentPr
 				top: `${position.y ?? 0}px`,
 			}}
 		>
-			<div class='bg-white p-3 rounded shadow flex flex-col w-96 gap-3'>
+			<div class='bg-white dark:bg-black p-3 rounded shadow flex flex-col w-96 gap-3'>
 				{props.children}
 			</div>
 		</div>
