d9078d1f551190e476c9a02e2f1454ebfbe84198
Will Nelson <me@wnelson.dev>
2023-01-17 20:41:41 -0800

    Move profile out of card

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,40 +1,29 @@
-import type { ReferenceElement } from '@floating-ui/dom';
-import { createSignal, JSX, lazy, Show, Suspense } from 'solid-js';
-import { Portal } from 'solid-js/web';
+import { For, JSX, Show } from 'solid-js';
 
 import useLanyard from '../lanyard/useLanyard';
-import SmallProfile from './SmallProfile';
-import StatusText from './StatusText';
-
-const FloatingCard = lazy(() => import('../FloatingCard'));
+import Game from './Game';
+import Spotify from './Spotify';
 
 const USER_ID = '618570414855028767';
 
 export default function Profile(): JSX.Element {
-	const [ref, setRef] = createSignal<ReferenceElement>();
-	const [cardVisible, setCardVisible] = createSignal<boolean>(false);
-
 	const presence = useLanyard(USER_ID);
 
 	return (
-		<div class='relative'>
-			<Show when={presence()} fallback={<span class='text-lg'>👀</span>}>
-				<span class='text-lg cursor-pointer' ref={setRef} onClick={() => setCardVisible(v => !v)}>
-					<StatusText presence={presence()!} />
-				</span>
+		<Show when={presence()}>
+			<Show when={presence()!.listening_to_spotify}>
+				<Spotify spotify={presence()!.spotify!} />
 			</Show>
-			<Show when={cardVisible() && presence()}>
-				<Suspense>
-					<FloatingCard ref={ref}>
-						<SmallProfile presence={presence()!} />
-					</FloatingCard>
-				</Suspense>
-			</Show>
-			<Show when={cardVisible()}>
-				<Portal>
-					<div class='fixed inset-0' onClick={() => setCardVisible(v => !v)} />
-				</Portal>
-			</Show>
-		</div>
-	)
+			<For each={presence()!.activities}>
+				{(item, index) => {
+					switch (item.type) {
+						case 0:
+							return <Game activity={item} />
+						default:
+							return <></>;
+					}
+				}}
+			</For>
+		</Show>
+	);
 }
