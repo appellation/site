6c201bb19ed25c38aeaebebbb6b1080e48638a04
Will Nelson <me@wnelson.dev>
2023-02-03 19:59:32 -0800

    Remove snake

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,6 +1,6 @@
-import { autoUpdate, offset, ReferenceElement, shift } from '@floating-ui/dom';
+import Dismiss from 'solid-dismiss';
 import { createMemo, createSignal, mergeProps, ParentProps, Show } from 'solid-js';
-import { useFloating } from './util/floating-ui';
+import FloatingCard from './FloatingCard';
 
 export interface Props {
 	links: Record<string, string>;
@@ -11,51 +11,40 @@ export default function Dropdown(props: ParentProps<Props>) {
 	props = mergeProps({ collapseSingle: true }, props);
 
 	const [isOpen, setOpen] = createSignal(false);
-	const [ref, setRef] = createSignal<ReferenceElement>();
-	const [floating, setFloating] = createSignal<HTMLElement>();
+	const [ref, setRef] = createSignal<HTMLElement>();
 	const firstLink = createMemo(() => Object.keys(props.links)[0]);
 	const hasManyLinks = createMemo(() => !Boolean(props.collapseSingle) || Object.keys(props.links).length > 1);
 
-	const position = useFloating(ref, floating, {
-		whileElementsMounted: autoUpdate,
-		middleware: [offset(5), shift()],
-		placement: 'bottom-start',
-	});
-
 	return (
-		<div onClick={() => setOpen(isOpen => hasManyLinks() && !isOpen)}>
-			<div ref={setRef} class='relative transition-colors hover:bg-stone-300 rounded px-1 cursor-pointer'>
+		<div class='inline-block relative'>
+			<div class='transition-colors hover:bg-stone-300 rounded px-1 cursor-pointer'>
 				<Show when={hasManyLinks()} fallback={<a href={firstLink()} rel="prefetch">{props.children}</a>}>
-					<div class='flex flex-row'>
+					<button ref={setRef} class='flex gap-2 items-center'>
 						<Show when={props.children}>
-							<span class='mr-2'>{props.children}</span>
+							<div>{props.children}</div>
 						</Show>
-						<span
-							class='inline-block self-center'
+						<div
 							classList={{
 								'i-bi-chevron-up': isOpen(),
 								'i-bi-chevron-down': !isOpen(),
 							}}
 						/>
-					</div>
+					</button>
 				</Show>
 			</div>
-			<div
-				ref={setFloating}
-				class='bg-stone-100 p-3 rounded shadow flex-col min-w-32'
-				classList={{ hidden: !isOpen(), flex: isOpen() }}
-				style={{
-					position: position.strategy,
-					left: `${position.x ?? 0}px`,
-					top: `${position.y ?? 0}px`,
-				}}
+			<Dismiss
+				menuButton={ref}
+				open={isOpen}
+				setOpen={setOpen}
 			>
-				<ul>
-					{Object.entries(props.links).map(([url, name]) => (
-						<a class='block transition-colors hover:bg-stone-200 rounded px-1' href={url} rel='prefetch'>{name}</a>
-					))}
-				</ul>
-			</div>
+				<FloatingCard ref={ref}>
+					<ul class='min-w-24'>
+						{Object.entries(props.links).map(([url, name]) => (
+							<a class='block transition-colors hover:bg-stone-200 rounded px-1' href={url} rel='prefetch'>{name}</a>
+						))}
+					</ul>
+				</FloatingCard>
+			</Dismiss>
 		</div>
 	);
 }
