52ec6dda8026bcde6af6e77871a6256487a0965d
Will Nelson <me@wnelson.dev>
2023-02-20 22:12:04 -0800

    Article OG tags

--- src/content/blog/2022/02/hello-world.md
+++ src/content/blog/2022/02/hello-world.md
@@ -1,5 +1,6 @@
 ---
 title: Hello World
+publishDate: 2022-20-02
 ---
 
 This is engaging content. You are engaged.
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ export const collections = {
 	'blog': defineCollection({
 		schema: z.object({
 			title: z.string(),
+			publishDate: z.string().transform(str => new Date(str)),
 		}),
 	}),
 }
