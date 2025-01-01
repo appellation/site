cc8945118ae7547c5a5d8f99a1b9231d03fe21c9
Will Nelson <me@wnelson.dev>
2025-01-01 14:02:15 -0800

    show subtitle in list

--- src/components/util/content.tsx
+++ src/components/util/content.tsx
@@ -5,3 +5,13 @@ export function isPublished({
 }) {
 	return draft !== true || import.meta.env.DEV;
 }
+
+export function postTitle({
+	title,
+	subtitle,
+}: {
+	title: string;
+	subtitle?: string;
+}) {
+	return title + (subtitle ? ` - ${subtitle}` : "");
+}
