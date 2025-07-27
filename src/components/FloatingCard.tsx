063905f9de35dcf0cdfd01463238047d613a17e0
Will Nelson <me@wnelson.dev>
2025-07-27 12:21:19 -0700

    add tidal card

--- src/components/FloatingCard.tsx
+++ src/components/FloatingCard.tsx
@@ -4,7 +4,7 @@ import { Dialog, Popover } from "react-aria-components";
 export default function FloatingCard({ children }: PropsWithChildren) {
 	return (
 		<Popover placement="bottom" offset={10}>
-			<Dialog className="bg-white dark:bg-black p-3 rounded shadow flex flex-co min-w-0 w-max max-w-96 gap-3">
+			<Dialog className="bg-white dark:bg-black p-3 rounded shadow flex min-w-0 w-max max-w-96 gap-3">
 				{children}
 			</Dialog>
 		</Popover>
--- src/components/status/TidalPill.tsx
+++ src/components/status/TidalPill.tsx
@@ -1,37 +1,76 @@
 import type { GatewayActivity } from "discord-api-types/v10";
 import { useEffect, useState } from "react";
 import Pill from "./Pill";
+import FloatingCard from "../FloatingCard";
+import { DialogTrigger } from "react-aria-components";
+import { DateTime, Duration } from "luxon";
 
 export type TidalPillProps = {
 	readonly activity: GatewayActivity;
 };
 
-export default function TidalPill(props: TidalPillProps) {
-	const duration =
-		props.activity.timestamps!.end! - props.activity.timestamps!.start!;
-	const calcSeconds = () => Date.now() - props.activity.timestamps!.start!;
+export default function TidalPill({ activity }: TidalPillProps) {
+	if (!activity.timestamps?.end || !activity.timestamps?.start) return null;
 
-	const [seconds, setSeconds] = useState(calcSeconds());
-	const progress = Math.min(seconds / duration, 1);
+	const duration = activity.timestamps.end - activity.timestamps.start;
+	const calcMs = () => Date.now() - activity.timestamps!.start!;
+
+	const [ms, setMs] = useState(calcMs());
+	const progress = Math.min(ms / duration, 1);
 
 	useEffect(() => {
-		setSeconds(calcSeconds());
-		const interval = setInterval(() => setSeconds(calcSeconds()), 1_000);
+		setMs(calcMs());
+		const interval = setInterval(() => setMs(calcMs()), 1_000);
 		return () => clearInterval(interval);
-	});
+	}, [activity.timestamps]);
 
+	activity.assets?.large_image;
 	return (
-		<div className="relative">
+		<DialogTrigger>
 			<Pill progress={progress}>
 				<span className="text-2xl i-simple-icons-tidal" />
 				<span className="w-full truncate">
-					<span>{props.activity.details}</span>
-					<span className="font-light text-sm">
-						{" "}
-						&mdash; {props.activity.state}
-					</span>
+					<span>{activity.details}</span>
+					<span className="font-light text-sm"> &mdash; {activity.state}</span>
 				</span>
 			</Pill>
-		</div>
+
+			<TidalCard
+				largeImage={activity.assets?.large_image}
+				title={activity.details!}
+				artist={activity.state!}
+				duration={Duration.fromMillis(ms)}
+				total={Duration.fromMillis(duration)}
+			/>
+		</DialogTrigger>
+	);
+}
+
+function TidalCard({
+	largeImage,
+	title,
+	artist,
+	duration,
+	total,
+}: {
+	largeImage?: string;
+	title: string;
+	artist: string;
+	duration: Duration;
+	total: Duration;
+}) {
+	const largeImageUrl = `https://media.discordapp.net/${largeImage?.slice(3)}`;
+
+	return (
+		<FloatingCard>
+			<img src={largeImageUrl} className="w-16 h-16 rounded" />
+			<div>
+				<p className="text-lg/4 font-bold">{title}</p>
+				<p>{artist}</p>
+				<p className="text-sm/4 font-light">
+					{duration.toFormat("mm:ss")} - {total.toFormat("mm:ss")}
+				</p>
+			</div>
+		</FloatingCard>
 	);
 }
